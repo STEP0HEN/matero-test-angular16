5a369423bce1346ff4e8638097dbb2bec195e7a5
STEP0HEN <guchy17@gmail.com>
2023-12-10 22:05:03 +0100

    Working matero test without standalone components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { data } from './data';
+import { MtxGrid, MtxGridColumn } from '@ng-matero/extensions/grid';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'matero-test';
+
+  @ViewChild('grid') grid!: MtxGrid;
+
+  columns: MtxGridColumn[] = [
+    {
+      header: 'Name',
+      field: 'name',
+      minWidth: 500,
+      maxWidth: 700,
+      resizable: true,
+    },
+    {
+      header: 'Weight',
+      field: 'weight',
+      minWidth: 500,
+      maxWidth: 700,
+      resizable: true,
+    },
+    { header: 'Gender', field: 'gender' },
+    { header: 'Mobile', field: 'mobile' },
+    { header: 'City', field: 'city' },
+  ];
+
+  list = data;
+
+  columnPinnable = false;
+  columnSortable = true;
+  columnHideable = true;
+  columnHideableChecked: 'show' | 'hide' = 'show';
+
+  closeMenu() {
+    this.grid.columnMenu.menuTrigger.closeMenu();
+  }
+
+  log(e: any) {
+    console.log(e);
+  }
 }
+
+/* 
+import { Component, ViewChild } from '@angular/core';
+import { EXAMPLE_DATA } from '../../data';
+import { MtxGridColumn, MtxGrid } from '@ng-matero/extensions/grid';
+
+@Component({
+  selector: 'data-grid-example',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+
+}
+*/
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { MtxGridModule } from '@ng-matero/extensions/grid';
+import { MtxSelectModule } from '@ng-matero/extensions/select';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonModule } from '@angular/material/button';
+import { MatRadioModule } from '@angular/material/radio';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MtxGridModule,
+    BrowserAnimationsModule,
+    MtxSelectModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    MatRadioModule,
+    CommonModule,
+    FormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
